754db30f5a47aee9b733bdf2e2e45dbc277ccd75
Envy <envy@gitsrv>
2020-11-30 06:58:19 +0100

    Added Class with Method IsValid to check ConteinerNumber + Tests

---
+++ Task2.xUnitTest/ConteinerNumberValidatorCheck.cs
@@ -0,0 +1,77 @@
+﻿using Xunit;
+
+namespace Task2.xUnitTest
+{
+    public class ConteinerNumberValidatorCheck
+    {
+        [Fact]
+        public void IsCodeValid_Empty()
+        {
+            ContainerNumberValidator cv = new ContainerNumberValidator();
+            Assert.False(cv.IsValid(""));
+        }
+
+        [Fact]
+        public void IsCodeValid_EmptySpace()
+        {
+            ContainerNumberValidator cv = new ContainerNumberValidator();
+            Assert.False(cv.IsValid(" "));
+        }
+
+        [Fact]
+        public void IsCodeValid_TFGU4112227()
+        {
+            ContainerNumberValidator cv = new ContainerNumberValidator();
+            Assert.True(cv.IsValid("TFGU4112227"));
+        }
+
+        [Fact]
+        public void IsCodeValid_tfgu4112227()
+        {
+            ContainerNumberValidator cv = new ContainerNumberValidator();
+            Assert.True(cv.IsValid("tfgu4112227"));
+        }
+
+        [Fact]
+        public void IsCodeValid_TFGU41122287()
+        {
+            ContainerNumberValidator cv = new ContainerNumberValidator();
+            Assert.False(cv.IsValid("TFGU41122287"));
+        }
+
+        [Fact]
+        public void IsCodeValid_TFGU4112227__()
+        {
+            ContainerNumberValidator cv = new ContainerNumberValidator();
+            Assert.True(cv.IsValid("TFGU4112227 "));
+        }
+
+        [Fact]
+        public void IsCodeValid_TFGU4_12227()
+        {
+            ContainerNumberValidator cv = new ContainerNumberValidator();
+            Assert.False(cv.IsValid("TFGU4$12227"));
+        }
+
+        [Fact]
+        public void IsCodeValid_TFGU411222B()
+        {
+            ContainerNumberValidator cv = new ContainerNumberValidator();
+            Assert.False(cv.IsValid("TFGU411222B"));
+        }
+
+        [Fact]
+        public void IsCodeValid_BLXM1122110()
+        {
+            ContainerNumberValidator cv = new ContainerNumberValidator();
+            Assert.False(cv.IsValid("BLXM1122110"));
+        }
+
+        [Fact]
+        public void IsCodeValid_BLXM1122212()
+        {
+            ContainerNumberValidator cv = new ContainerNumberValidator();
+            Assert.True(cv.IsValid("BLXM1122212"));
+        }
+    }
+}
---
+++ Task2/ContainerNumberValidator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Task2
+{
+    public class ContainerNumberValidator : IValidator
+    {
+        readonly ICharDecoder alphasymbolDecoder;
+        readonly ICharDecoder numberDecoder;
+        int digitsNumber = 11;
+
+        public ContainerNumberValidator()
+        {
+            alphasymbolDecoder = new AlphasymbolDecoder();
+            numberDecoder = new NumberDecoder();
+        }
+
+        public ContainerNumberValidator(int digitsNumber) : this()
+        {
+            this.digitsNumber = digitsNumber;
+        }
+
+        public bool IsValid(string input)
+        {            
+            if (String.IsNullOrEmpty(input))
+            {
+                // throw new ArgumentNullException("Checked string could not be empty");
+                // TODO: write to log: "Checked string could not be empty"
+                return false;
+            }
+
+            string trimmedInput = input.Trim().ToUpper();
+            if (trimmedInput.Length != digitsNumber)
+            {
+                // throw new ArgumentException($"Incorrect lenghts of input :'{trimmedInput}'");
+                // TODO: write to log: $"Incorrect lenghts of input :'{trimmedInput}'"
+                return false;
+            }
+
+            if (!Char.IsDigit(trimmedInput[trimmedInput.Length - 1]))
+            {
+                // throw new ArgumentException($"Last element in input should be Digit :'{trimmedInput}'");
+                // TODO: write to log: $"Last element in input should be Digit :'{trimmedInput}'"
+                return false;
+
+            }
+
+            double summ = CalculateSumm(trimmedInput);
+            int devideResult = (int)summ / digitsNumber;
+            int multiplyResult = devideResult * digitsNumber;
+
+            return (summ - multiplyResult == numberDecoder.Decode(trimmedInput[trimmedInput.Length - 1]));
+        }
+
+        private double CalculateSumm(string trimmedInput)
+        {
+            double summ = 0;
+            for (int i = 0; i < trimmedInput.Length - 1; i++)
+            {
+                char symbol = trimmedInput[i];
+                if (Char.IsLetterOrDigit(symbol))
+                {
+                    summ += (Char.IsDigit(symbol)
+                            ? numberDecoder.Decode(symbol)
+                            : alphasymbolDecoder.Decode(symbol)) * (Math.Pow(2, i));
+
+                }
+                else
+                {
+                    // throw new ArgumentException($"Used Incorrect symbol '{trimmedInput[i]}' at position: {i} in input '{trimmedInput}'");
+                    // TODO: write to log: $"Used Incorrect symbol '{trimmedInput[i]}' at position: {i} in input '{trimmedInput}'"
+                }
+            }
+
+            return summ;
+        }
+    }
+}
