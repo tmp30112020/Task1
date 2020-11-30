b35f8545a7a87b86c4c06b025d41d11001b42d58
Envy <envy@gitsrv>
2020-11-30 06:32:04 +0100

    Added class for decoding Digit Symbol to number

---
+++ Task2/NumberDecoder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task2
+{
+    class NumberDecoder : ICharDecoder
+    {
+        public int Decode(char character)
+        {
+            return (int)Char.GetNumericValue(character);
+        }
+    }
+}
