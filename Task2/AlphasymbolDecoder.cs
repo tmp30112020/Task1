dda525ce35ac3e41066f6ce24ecbc74af318c031
Envy <envy@gitsrv>
2020-11-30 06:35:51 +0100

    Added class for Alphabetic characters decode to number value

---
+++ Task2/AlphasymbolDecoder.cs
@@ -0,0 +1,12 @@
+﻿namespace Task2
+{
+    class AlphasymbolDecoder : ICharDecoder
+    {
+        public int Decode(char character)
+        {
+            int pos = (int)character % 32 - 1;
+            var calc = (pos - 1) / 10 + ((pos - 1) < 0 ? 0 : 1);
+            return (pos + 10 + calc);
+        }
+    }
+}
